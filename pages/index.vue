d9876f9a5e56ae19a7e15620ab96902f65e6fb58
Renan Bioli <49667341+biolii@users.noreply.github.com>
2024-12-06 09:33:34 -0300

    HTML + CSS

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,40 @@
 
 
 <template>
-  <p>
-    DESIGN SYSTEM PGDF
-  </p>
+  <div class="index-box2">
+    <h1>Componentes</h1>
+    <p>Componentes são blocos de construção interativos <br>
+       para a criação de uma interface de usuário. Eles <br>
+       podem ser organizados em categorias com base <br>
+       na sua finalidade: Ação, contenção, comunicação, <br>
+       navegação, seleção e entrada de texto.</p>
+  </div>
+  <div class="index-buttons">
+    <div class="index-box">
+      <div class="box-image">
+        <img src="/assets/img/index imgs/Btns.svg" class="img1" alt="">
+        <img src="/assets/img/index imgs/btn hober.svg" alt="">
+      </div>
+     <p>Botões</p>
+     <p>Todos os botões usados na UI</p>
+    </div>
+    <div class="index-box">
+      <div class="box-image">
+        <img src="/assets/img/index imgs/Icons.svg" alt="">
+      </div>
+     <p>Icones</p>
+     <p>Icones que ajudam a realizar tarefas menores com apenas 
+      um clique</p>
+    </div>
+    <div class="index-box">
+      <div class="box-image">
+        <img src="/assets/img/index imgs/ActBtn.svg" class="img1" alt="">
+        <img src="/assets/img/index imgs/ActBtn hover.svg" alt="">
+      </div>
+     <p>Botões de ação</p>
+     <p>Botões usados para ações primárias</p>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
