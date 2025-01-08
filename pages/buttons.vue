54df016c7b61df778a9608f51552f7c479a813b0
Renan Bioli <49667341+biolii@users.noreply.github.com>
2025-01-08 09:52:37 -0300

    buttons e css

---
+++ pages/buttons.vue
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+
+<template>
+  <div class="index-box3">
+    <p>Botôes</p>
+  </div>
+  <div class="index-box4">
+   <button class="btn-style ">Elevado</button>
+   <button class="btn-style">Preenchido</button>
+   <button class="btn-style btn-alternativo">Alternativo</button>
+   <button class="btn-traçado">Traçado</button>
+   <button class="btn-style">Texto</button>
+  </div>
+</template>
+
+<style lang="scss">
+
+@import '/assets/styles/theme.scss';
+
+</style>
